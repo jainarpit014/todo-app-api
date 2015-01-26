09fc6c40cf673a3183cf16d55067b3d253f9a9c8
root <root@ip-172-31-15-113.ap-southeast-1.compute.internal>
2015-01-26 20:07:18 +0000

    canged routes from any to post

--- app/routes.php
+++ app/routes.php
@@ -30,13 +30,13 @@ Route::get('tasks',['before'=>'oauth','uses'=>  function()
     return Comments::all();
 });*/
 
-Route::any('gettasks', ['before'=>'oauth','uses'=>'TaskController@getTasksWithFlag']);
+Route::post('gettasks', ['before'=>'oauth','uses'=>'TaskController@getTasksWithFlag']);
 Route::post('savetask',['before'=>'oauth','uses'=>'TaskController@add']);
-Route::any('updatetaskflag',['before'=>'oauth','uses'=>'TaskController@updateTaskFlag']);
-Route::any('updatetask',['before'=>'oauth','uses'=>'TaskController@updateTask']);
-Route::any('savecomment',['before'=>'oauth','uses'=>'CommentController@add']);
-Route::any('getcomments',['before'=>'oauth','uses'=>'CommentController@getComments']);
-Route::any('sendmail',['before'=>'oauth','uses'=>'TaskController@emailTask']);
+Route::post('updatetaskflag',['before'=>'oauth','uses'=>'TaskController@updateTaskFlag']);
+Route::post('updatetask',['before'=>'oauth','uses'=>'TaskController@updateTask']);
+Route::post('savecomment',['before'=>'oauth','uses'=>'CommentController@add']);
+Route::post('getcomments',['before'=>'oauth','uses'=>'CommentController@getComments']);
+Route::post('sendmail',['before'=>'oauth','uses'=>'TaskController@emailTask']);
 Route::post('oauth/access_token', 'OAuthController@accessToken');
-Route::any('createapp',['before'=>'auth.basic','uses'=>'UserController@createApp']);
+Route::post('createapp',['before'=>'auth.basic','uses'=>'UserController@createApp']);
 Route::post('oauth/id_secret','OAuthController@generateIdSecret');
